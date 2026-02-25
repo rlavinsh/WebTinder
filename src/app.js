98c17dfd5c1aa408dac82ce111309b6bc367e067
rlavinsh <rlavinsh@gmail.com>
2026-02-25 16:29:35 +0530

    handling the multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,36 +2,19 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("Hello Hello Hello");
-// });
-// app.use("/kaise", (req, res) => {
-//   res.send("server toh yha bhi chalu hein");
-// });
-
-// this will only handle get call to /user
-app.get("/user", (req, res) => {
-  res.send({
-    firstName: "Lavinsh",
-    lastName: "Rathor",
-    phone: "51454646546",
-    isAdmin: "false",
-  });
-});
-
-app.post("/user", (req, res) => {
-  // console.log("Save data to the database");
-  res.send("Data saved sucessfully");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Delete the user successfully");
-});
-// this will match all the http methods API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
-
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route");
+    next();
+    // res.send("Response!!");
+  },
+  // second handler
+  (req, res) => {
+    console.log("handling the second route");
+    res.send("Response 2!!");
+  },
+);
 app.listen(3000, (req, res) => {
   //   res.end("Hello");
   console.log("server chal rha hein port number 3000 per");
